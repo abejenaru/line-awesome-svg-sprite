14f2b0598f52626e47c8bba5b97cccd827ea7009
Andrei B <abejenaru@gmail.com>
2021-11-11 10:03:10 +0100

    Code cleanup

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 'use strict';
 
-const { dirname } = require('path');
 const SVGSpriter    = require('svg-sprite'),
-yaml                = require('js-yaml'),
-path                = require('path'),
-mkdirp              = require('mkdirp'),
-fs                  = require('fs');
+    yaml            = require('js-yaml'),
+    path            = require('path'),
+    mkdirp          = require('mkdirp'),
+    fs              = require('fs');
 
-const config = {
-    "svg": {
-        "xmlDeclaration": false,
-        "doctypeDeclaration": false,
-        "namespaceIDs": false,
-        "namespaceClassnames": false
-    },
-    "mode": {
-        "symbol": {
-            "dest": ".",
-            "sprite": "line-awesome-icons.svg"
+
+var configs = {
+    icons: {
+        "svg": {
+            "xmlDeclaration": false,
+            "doctypeDeclaration": false,
+            "namespaceIDs": false,
+            "namespaceClassnames": false
+        },
+        "mode": {
+            "symbol": {
+                "dest": ".",
+                "sprite": "line-awesome-icons.svg"
+            }
         }
-    }
-},
-config_brands = {
-    "svg": {
-        "xmlDeclaration": false,
-        "doctypeDeclaration": false,
-        "namespaceIDs": false,
-        "namespaceClassnames": false
     },
-    "mode": {
-        "symbol": {
-            "dest": ".",
-            "sprite": "line-awesome-brands.svg"
+    brands: {
+        "svg": {
+            "xmlDeclaration": false,
+            "doctypeDeclaration": false,
+            "namespaceIDs": false,
+            "namespaceClassnames": false
+        },
+        "mode": {
+            "symbol": {
+                "dest": ".",
+                "sprite": "line-awesome-brands.svg"
+            }
         }
     }
 };
@@ -42,57 +44,52 @@ const yamlData  = yaml.load(fs.readFileSync(__dirname + '/brands.yaml', 'utf-8')
 const SVG_DIR   = __dirname + '/node_modules/line-awesome/svg/';
 const files     = fs.readdirSync(SVG_DIR);
 
-const spriter = new SVGSpriter(config),
-spriter_brands= new SVGSpriter(config_brands);
+var spriters = {
+    icons: new SVGSpriter(configs.icons),
+    brands: new SVGSpriter(configs.brands)
+};
 
 // Register SVG files with the spriter
-files.forEach(function (file) {
+files.forEach(function(file){
     if (yamlData.Brands.includes(file.replace('.svg',''))) {
-        spriter_brands.add(
+        spriters.brands.add(
             path.resolve(SVG_DIR + file),
             file,
             fs.readFileSync(path.resolve(SVG_DIR + file), {encoding: 'utf-8'})
         );
     } else {
-        spriter.add(
+        spriters.icons.add(
             path.resolve(SVG_DIR + file),
             file,
             fs.readFileSync(path.resolve(SVG_DIR + file), {encoding: 'utf-8'})
         );
     }
-})
+});
+
 
 // Compile the sprites
-spriter.compile(function(error, result, cssData) {
-    // Run through all configured output modes
-    for (var mode in result) {
-        // Run through all created resources and write them to disk
-        for (var type in result[mode]) {
-            mkdirp.sync(path.dirname(result[mode][type].path));
-            fs.writeFileSync(result[mode][type].path, result[mode][type].contents);
-            cleanupSprite(result[mode][type].path);
-        }
-    }
-});
+for (const prop in spriters) {
+    const sprinter = spriters[prop];
 
-spriter_brands.compile(function(error, result, cssData) {
-    // Run through all configured output modes
-    for (var mode in result) {
-        // Run through all created resources and write them to disk
-        for (var type in result[mode]) {
-            mkdirp.sync(path.dirname(result[mode][type].path));
-            fs.writeFileSync(result[mode][type].path, result[mode][type].contents);
-            cleanupSprite(result[mode][type].path);
+    sprinter.compile(function(error, result, cssData) {
+        // Run through all configured output modes
+        for (const mode in result) {
+            // Run through all created resources and write them to disk
+            for (const type in result[mode]) {
+                mkdirp.sync(path.dirname(result[mode][type].path));
+                fs.writeFileSync(result[mode][type].path, result[mode][type].contents);
+                cleanupSprite(result[mode][type].path);
+            }
         }
-    }
-});
+    });
+}
 
 
 function cleanupSprite (spriteFile)
 {
-    var fileContent = fs.readFileSync(spriteFile, 'utf8');
+    const fileContent = fs.readFileSync(spriteFile, 'utf8');
 
-    var result = fileContent.replaceAll(' xmlns="http://www.w3.org/2000/svg"', '')
+    const result = fileContent.replaceAll(' xmlns="http://www.w3.org/2000/svg"', '')
                             .replaceAll(' xmlns:xlink="http://www.w3.org/1999/xlink"', '')
                             .replace('<svg>', '<svg xmlns="http://www.w3.org/2000/svg">');
     
